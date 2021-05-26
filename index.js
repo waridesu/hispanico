009a89321783e63df731406cc52e8ed67b39475d
Denis <waridesu@gmail.com>
2021-05-26 05:50:56 +0300

    test site

---
+++ index.js
@@ -0,0 +1,8 @@
+const headerBurger = document.querySelector('.burger');
+const mobileMenu = document.querySelector('nav');
+
+headerBurger.addEventListener('click', function () {
+    mobileMenu.classList.toggle("show")
+})
+
+
