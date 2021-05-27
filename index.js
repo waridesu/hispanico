a948bc3f7cd82d9720c2467c4f155a30d0d3266d
Denis <waridesu@gmail.com>
2021-05-27 17:55:49 +0300

    style & image

--- index.js
+++ index.js
@@ -5,4 +5,39 @@ headerBurger.addEventListener('click', function () {
     mobileMenu.classList.toggle("show")
 })
 
-
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('.first-slider', {
+        type: 'loop',
+        pagination: false,
+        perPage: 5,
+        arrows: {
+            height: '88px',
+            width: '44px',
+        },
+        breakpoints: {
+            1200: {
+                perPage: 3,
+            },
+            768: {
+                perPage: 1,
+            }
+        },
+    }).mount();
+    new Splide('.second-slider', {
+        type: 'loop',
+        pagination: false,
+        perPage: 5,
+        arrows: {
+            height: '88px',
+            width: '44px',
+        },
+        breakpoints: {
+            1200: {
+                perPage: 3,
+            },
+            768: {
+                perPage: 1,
+            }
+        },
+    }).mount();
+});
